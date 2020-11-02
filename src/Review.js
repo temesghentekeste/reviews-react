d299086abe1348e84c89dd17c50dfe4a5ac45423
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-11-03 02:18:29 +0300

    Add review component, add css file, setup basic project structure

---
+++ src/Review.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaGithubSquare, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  return <h2>
+    review component <FaGithubSquare/>
+    </h2>;
+};
+
+export default Review;
