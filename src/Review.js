85370c24ac3824333a45632968e4b90d52813813
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-11-05 02:12:11 +0300

    Add basic reviews setup

--- src/Review.js
+++ src/Review.js
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
 import people from './data';
-import { FaChevronLeft, FaChevronRight, FaGithubSquare, FaQuoteRight } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>
-    review component <FaGithubSquare/>
-    </h2>;
+  const [index, setIndex] = useState(0);
+  const {name, job, image, text} = people[index];
+  return(
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className='person-img'/>
+        <span className="quote-icon">
+          <FaQuoteRight/>
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn"><FaChevronLeft/></button>
+        <button className="next-btn"><FaChevronRight/></button>
+        <button className="random-btn">surprise me</button>
+      </div>
+    </article>
+    ) 
 };
 
 export default Review;
